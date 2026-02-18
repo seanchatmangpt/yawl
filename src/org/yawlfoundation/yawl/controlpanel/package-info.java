103e94bfb904b0da502d3156d500d47a8ff5f370
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-02-17 18:08:30 -0800

    Quicksave.

---
+++ src/org/yawlfoundation/yawl/controlpanel/package-info.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2004-2025 The YAWL Foundation. All rights reserved.
+ * The YAWL Foundation is a collaboration of individuals and
+ * organisations who are committed to improving workflow technology.
+ *
+ * This file is part of YAWL. YAWL is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation.
+ *
+ * YAWL is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with YAWL. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Desktop control panel application for YAWL engine management.
+ *
+ * <p>This package provides a Swing-based desktop application for administering
+ * YAWL workflow engines. It enables users to deploy specifications, monitor
+ * running cases, manage work items, and configure engine settings.</p>
+ *
+ * <p>Key features:</p>
+ * <ul>
+ *   <li>Engine connection management and status monitoring</li>
+ *   <li>Specification deployment and version management</li>
+ *   <li>Case instance monitoring and control</li>
+ *   <li>Work item administration</li>
+ *   <li>Resource participant management</li>
+ *   <li>Log viewing and analysis</li>
+ * </ul>
+ *
+ * <p>The control panel connects to YAWL engines via the REST/HTTP interfaces
+ * (Interface A and Interface B) and requires appropriate authentication
+ * credentials.</p>
+ *
+ * @see org.yawlfoundation.yawl.controlpanel.YControlPanel
+ * @see org.yawlfoundation.yawl.engine.YEngine
+ */
+package org.yawlfoundation.yawl.controlpanel;
